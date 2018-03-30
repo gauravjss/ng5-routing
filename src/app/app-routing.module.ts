976560a4c3595832a247d5f7c738ba0d594ad5d1
Gaurav Srivastava <gauravjss@gmail.com>
2018-03-29 23:27:42 -0500

    Modifying Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,9 @@ import {HomeComponent} from './home/home.component';
 import {UserComponent} from './users/user/user.component';
 import {UsersComponent} from './users/users.component';
 import {AuthGuardService} from './auth-guard-service';
+import {CanDeactivateGuard} from './can-deactivate-guard.service';
+import {ErrorPageComponent} from './error-page/error-page.component';
+import {ServerResolver} from './servers/server/server-resolver.service';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
@@ -20,14 +23,17 @@ const appRoutes: Routes = [
     canActivateChild: [AuthGuardService],
     component: ServersComponent,
     children: [
-      {path: ':id', component: ServerComponent},
-      {path: ':id/edit', component: EditServerComponent}
+      {path: ':id', component: ServerComponent, resolve: {server: ServerResolver}},
+      {path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard]}
     ]},
-  {path: 'not-found' , component: PageNotFoundComponent},
+  // {path: 'not-found' , component: PageNotFoundComponent},
+  {path: 'not-found' , component: ErrorPageComponent,
+    data: {message: 'Page not found !'}},
   {path: '**' , redirectTo: '/not-found'},
 ];
 
 @NgModule({
+  // imports: [RouterModule.forRoot(appRoutes, {useHash: true})],
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
